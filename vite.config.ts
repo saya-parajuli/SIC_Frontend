458a89e07a685b28e4964eeeed7d741ad551d144
saya parajuli <susayaparajuli@gmail.com>
2026-05-14 23:49:33 +0545

    frontend initialized

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
-});
+  plugins: [
+    tsconfigPaths(),
+    tanstackStart(),
+    react(),
+  ],
+})
